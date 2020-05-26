cb6a6b48bde676f28fbc68eb0b969fa056e78a4f
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2020-05-26 14:20:34 +0200

    Add project directory layout and a test stub

---
+++ test/test_ta.cpp
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *  test_ta.cpp - Test TA implementation
+ *
+ *  Created: Tue 26 May 2020 13:51:10 CEST 13:51
+ *  Copyright  2020  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+TEST_CASE("Stub", "[libta]")
+{
+	REQUIRE(1 == 1);
+}
