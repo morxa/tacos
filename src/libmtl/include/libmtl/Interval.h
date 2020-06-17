293aae0daafd9990e077e4425aa5d700d3d974ba
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-06-17 15:10:42 +0200

    libmtl: add documentation.

--- src/libmtl/include/libmtl/Interval.h
+++ src/libmtl/include/libmtl/Interval.h
@@ -4,28 +4,62 @@ namespace arithmetic {
 
 enum class BoundType { WEAK, STRICT, INFTY };
 
+/**
+ * @brief Templated class for intervals with a reduced set of operations.
+ *
+ * @tparam N The used number type, relevant for comparisons and arithmetic operations.
+ */
 template <typename N>
 class Interval
 {
 public:
+	/**
+	 * @brief Construct a new Interval object
+	 */
 	Interval() = default;
 
+	/**
+	 * @brief Construct a new Interval object from bounds with implicit weak bound types
+	 *
+	 * @param lb
+	 * @param up
+	 */
 	Interval(N lb, N up)
 	: lower_(lb), upper_(up), lowerBoundType_(BoundType::WEAK), upperBoundType_(BoundType::WEAK)
 	{
 	}
 
+	/**
+	 * @brief Construct a new Interval object from bounds and bound types
+	 *
+	 * @param lb
+	 * @param lbType
+	 * @param up
+	 * @param upType
+	 */
 	Interval(N lb, BoundType lbType, N up, BoundType upType)
 	: lower_(lb), upper_(up), lowerBoundType_(lbType), upperBoundType_(upType)
 	{
 	}
 
+	/**
+	 * @brief Checks containment of a value within the interval
+	 * @param value
+	 * @return true
+	 * @return false
+	 */
 	bool
 	contains(const N &value) const
 	{
 		return fitsLower(value) && fitsUpper(value);
 	}
 
+	/**
+	 * @brief Checks for emptiness
+	 *
+	 * @return true
+	 * @return false
+	 */
 	bool
 	is_empty() const
 	{
@@ -37,24 +71,44 @@ public:
 		                   && lowerBoundType_ != BoundType::INFTY)));
 	}
 
+	/**
+	 * @brief Getter for lower bound
+	 *
+	 * @return const N&
+	 */
 	const N &
 	lower() const noexcept
 	{
 		return lower_;
 	}
 
+	/**
+	 * @brief Getter for upper bound
+	 *
+	 * @return const N&
+	 */
 	const N &
 	upper() const noexcept
 	{
 		return upper_;
 	}
 
+	/**
+	 * @brief Getter for lower bound type
+	 *
+	 * @return BoundType
+	 */
 	BoundType
 	lowerBoundType() const noexcept
 	{
 		return lowerBoundType_;
 	}
 
+	/**
+	 * @brief Getter for upper bound type
+	 *
+	 * @return BoundType
+	 */
 	BoundType
 	upperBoundType() const noexcept
 	{
--- src/libmtl/include/libmtl/MTLFormula.h
+++ src/libmtl/include/libmtl/MTLFormula.h
@@ -15,35 +15,65 @@ using TimeInterval = ::arithmetic::Interval<TimePoint>;
 
 class MTLFormula;
 
+/**
+ * @brief Strong typing of atomic propositions, allows simplified construction from Boolean values
+ * as well.
+ *
+ */
 struct AtomicProposition
 {
 	AtomicProposition() = delete;
 
+	/**
+	 * @brief Construct a new Atomic Proposition object from string
+	 *
+	 * @param name
+	 */
 	AtomicProposition(const std::string &name) : ap_(name)
 	{
 	}
 
+	/**
+	 * @brief Construct a new Atomic Proposition object from bool
+	 *
+	 * @param b
+	 */
 	template <typename Y, typename T = std::enable_if_t<std::is_same<Y, bool>{}>>
 	AtomicProposition(Y b)
 	{
 		ap_ = b ? "true" : "false";
 	}
 
-	std::string ap_;
+	std::string ap_; ///< String representation of the ap
 };
 
+/// Comparison override
 bool operator==(const AtomicProposition &lhs, const AtomicProposition &rhs);
 
+/**
+ * @brief A (timed-) word which can be validated against a given MTL-formula.
+ *
+ */
 class MTLWord
 {
 public:
 	friend class MTLFormula;
 
+	/**
+	 * @brief Constructor
+	 */
 	MTLWord(std::initializer_list<std::pair<std::vector<AtomicProposition>, TimePoint>> a) : word_(a)
 	{
 	}
 
+	/**
+	 * @brief Checks satisfaction at a certain position
+	 */
 	bool satisfies_at(const MTLFormula &phi, std::size_t i) const;
+
+	/**
+	 * @brief Checks satisfaction at pos 0
+	 */
 	bool satisfies(const MTLFormula &phi) const;
 
 private:
@@ -52,24 +82,39 @@ private:
 
 enum class LOP { LAND, LOR, LNEG, LUNTIL, AP };
 
+/**
+ * @brief Class representing an MTL-formula with the usual operators.
+ *
+ */
 class MTLFormula
 {
 public:
 	friend class MTLWord;
 	MTLFormula() = delete;
+
+	/**
+	 * @brief Constructor from atomic proposition
+	 */
 	MTLFormula(const AtomicProposition &ap);
 
+	/**
+	 * @brief Constructor from bool
+	 */
 	template <typename Y, typename T = std::enable_if_t<std::is_same<Y, bool>{}>>
 	MTLFormula(Y b) : ap_(b), operator_(LOP::AP)
 	{
 	}
 
+	/** Boolean AND-operator **/
 	MTLFormula operator&&(const MTLFormula &rhs) const;
+	/** Boolean OR-operator **/
 	MTLFormula operator||(const MTLFormula &rhs) const;
+	/** Boolean negation-operator **/
 	MTLFormula operator!() const;
-
+	/// timed-until operator (binary)
 	MTLFormula until(const MTLFormula &rhs, const TimeInterval &duration = TimeInterval()) const;
 
+	/// function to test whether a formula consists solely of an atomic proposition
 	bool
 	is_AP() const
 	{
