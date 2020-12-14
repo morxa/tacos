956ec8bb54f520b4f5413e83ba406091dc2801c7
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-12-14 16:34:28 +0100

    add function to test for almostZero

--- src/utilities/include/utilities/numbers.h
+++ src/utilities/include/utilities/numbers.h
@@ -1,7 +1,16 @@
 #pragma once
 #include <cmath>
+#include "config.h"
 
 namespace utilities {
+
+
+
+/// checks whether a float is close to zero, based on https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
+template<typename Float>
+bool isNearZero(Float in, int factor = absolute_epsilon_factor) {
+    return abs(in) < factor*std::numeric_limits<Float>::epsilon();
+}
 
 template<typename Integer, typename Float>
 Integer getIntegerPart(Float in) {
@@ -15,7 +24,7 @@ Float getFractionalPart(Float in) {
 
 template<typename Integer, typename Float>
 bool isInteger(Float in) {
-    return getFractionalPart<Integer,Float>(in) == Float(0);
+    return isNearZero(getFractionalPart<Integer,Float>(in));
 }
 
 }
