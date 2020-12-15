78bc685e15ac7b0958248371db9fbde889a3854f
Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
2020-12-15 16:37:53 +0100

    update configuration for float-comparison tolerances

--- src/utilities/include/utilities/config.h
+++ src/utilities/include/utilities/config.h
@@ -3,7 +3,7 @@
 namespace utilities
 {
     /// defines how many multiples of std::epsilon are used for absolute comparison to zero
-    static const int absolute_epsilon_factor = 4;
+    static const int absolute_epsilon_factor = 8;
     /// defines how many ulps (units in the last place) are used for float comparison
-    static const int max_ulps = 4;
+    static const int max_ulps = 8;
 } // namespace utilities
