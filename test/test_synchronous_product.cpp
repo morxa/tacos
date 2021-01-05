f84d0113c7c54d2add44582381620b315a3a8030
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-01-05 14:36:23 +0100

    Extend canonical word test with another, more complex test case

--- test/test_synchronous_product.cpp
+++ test/test_synchronous_product.cpp
@@ -35,6 +35,7 @@ using automata::ta::ClockSetValuation;
 using synchronous_product::ABRegionSymbol;
 using synchronous_product::ATAConfiguration;
 using synchronous_product::ATARegionState;
+using synchronous_product::get_canonical_word;
 using synchronous_product::TARegionState;
 
 TEST_CASE("Print a TARegionState", "[print]")
@@ -78,7 +79,7 @@ TEST_CASE("Print a set of ABRegionSymbols (one Abs_i)", "[print]")
 		const std::set<ABRegionSymbol<std::string, std::string>> word;
 		std::stringstream                                        stream;
 		stream << word;
-		REQUIRE(stream.str() == "{}");
+		CHECK(stream.str() == "{}");
 	}
 	{
 		std::set<ABRegionSymbol<std::string, std::string>> word{
@@ -87,7 +88,7 @@ TEST_CASE("Print a set of ABRegionSymbols (one Abs_i)", "[print]")
 		    logic::MTLFormula<std::string>{logic::AtomicProposition<std::string>{"s"}}, 2)};
 		std::stringstream stream;
 		stream << word;
-		REQUIRE(stream.str() == "{ (s, c, 1), (s, 2) }");
+		CHECK(stream.str() == "{ (s, c, 1), (s, 2) }");
 	}
 }
 
@@ -97,7 +98,7 @@ TEST_CASE("Print the canonical word H(s)", "[print]")
 		const std::vector<std::set<ABRegionSymbol<std::string, std::string>>> word;
 		std::stringstream                                                     stream;
 		stream << word;
-		REQUIRE(stream.str() == "[]");
+		CHECK(stream.str() == "[]");
 	}
 	{
 		std::vector<std::set<ABRegionSymbol<std::string, std::string>>> word;
@@ -108,7 +109,7 @@ TEST_CASE("Print the canonical word H(s)", "[print]")
 		{
 			std::stringstream stream;
 			stream << word;
-			REQUIRE(stream.str() == "[ { (s, c, 1), (s, 2) } ]");
+			CHECK(stream.str() == "[ { (s, c, 1), (s, 2) } ]");
 		}
 		word.push_back(std::set<ABRegionSymbol<std::string, std::string>>(
 		  {TARegionState<std::string>("s", "c2", 5),
@@ -117,39 +118,78 @@ TEST_CASE("Print the canonical word H(s)", "[print]")
 		{
 			std::stringstream stream;
 			stream << word;
-			REQUIRE(stream.str() == "[ { (s, c, 1), (s, 2) }, { (s, c2, 5), (a, 3) } ]");
+			CHECK(stream.str() == "[ { (s, c, 1), (s, 2) }, { (s, c2, 5), (a, 3) } ]");
 		}
 		word.push_back(std::set<ABRegionSymbol<std::string, std::string>>(
 		  {TARegionState<std::string>("s2", "c3", 10)}));
 		{
 			std::stringstream stream;
 			stream << word;
-			REQUIRE(stream.str()
-			        == "[ { (s, c, 1), (s, 2) }, { (s, c2, 5), (a, 3) }, { (s2, c3, 10) } ]");
+			CHECK(stream.str() == "[ { (s, c, 1), (s, 2) }, { (s, c2, 5), (a, 3) }, { (s2, c3, 10) } ]");
 		}
 	}
 }
 
-TEST_CASE("Get a canonical word of a simple state", "[sproduct]")
+TEST_CASE("Get a canonical word of an empty", "[canonical_word]")
 {
-	logic::MTLFormula             f{logic::AtomicProposition<std::string>{"a"}};
-	ATAConfiguration<std::string> ata_configuration = {{f, 0.0}};
-	ClockSetValuation             v;
-	v["c"] = 0;
-	automata::ta::Configuration<std::string> ta_configuration{"s", v};
-	auto w = synchronous_product::get_canonical_word<std::string, std::string>(ta_configuration,
-	                                                                           ata_configuration,
-	                                                                           5);
+	auto w = get_canonical_word<std::string, std::string>({}, {}, 3);
+	INFO("Canonical word: " << w);
+	REQUIRE(w.size() == 0);
+}
+
+TEST_CASE("Get a canonical word of a simple state", "[canonical_word]")
+{
+	const logic::MTLFormula                        f{logic::AtomicProposition<std::string>{"a"}};
+	const ATAConfiguration<std::string>            ata_configuration = {{f, 0.0}};
+	const ClockSetValuation                        v{{"c", 0}};
+	const automata::ta::Configuration<std::string> ta_configuration{"s", v};
+	const auto                                     w =
+	  get_canonical_word<std::string, std::string>(ta_configuration, ata_configuration, 5);
 	INFO("Canonical word: " << w);
 	REQUIRE(w.size() == 1);
 	const auto &abs1 = *w.begin();
 	REQUIRE(abs1.size() == 2);
 	const auto &symbol1 = *abs1.begin();
 	REQUIRE(std::holds_alternative<TARegionState<std::string>>(symbol1));
-	REQUIRE(std::get<TARegionState<std::string>>(symbol1) == TARegionState<std::string>("s", "c", 0));
+	CHECK(std::get<TARegionState<std::string>>(symbol1) == TARegionState<std::string>("s", "c", 0));
 	const auto &symbol2 = *std::next(abs1.begin());
 	REQUIRE(std::holds_alternative<ATARegionState<std::string>>(symbol2));
-	REQUIRE(std::get<ATARegionState<std::string>>(symbol2) == ATARegionState<std::string>(f, 0));
+	CHECK(std::get<ATARegionState<std::string>>(symbol2) == ATARegionState<std::string>(f, 0));
+}
+
+TEST_CASE("Get a canonical word of a more complex state", "[canonical_word]")
+{
+	const logic::MTLFormula                        a{logic::AtomicProposition<std::string>{"a"}};
+	const logic::MTLFormula                        b{logic::AtomicProposition<std::string>{"b"}};
+	const ATAConfiguration<std::string>            ata_configuration = {{a, 0.5}, {b, 1.5}};
+	const ClockSetValuation                        v{{"c1", 0.1}, {"c2", 0.5}};
+	const automata::ta::Configuration<std::string> ta_configuration{"s", v};
+	const auto                                     w =
+	  get_canonical_word<std::string, std::string>(ta_configuration, ata_configuration, 3);
+	INFO("Canonical word: " << w);
+	REQUIRE(w.size() == 2);
+	{
+		const auto &abs1 = *w.begin();
+		REQUIRE(abs1.size() == 1);
+		const auto &symbol1 = *abs1.begin();
+		REQUIRE(std::holds_alternative<TARegionState<std::string>>(symbol1));
+		CHECK(std::get<TARegionState<std::string>>(symbol1)
+		      == TARegionState<std::string>("s", "c1", 1));
+	}
+	{
+		const auto &abs2 = *std::next(w.begin());
+		REQUIRE(abs2.size() == 3);
+		const auto &symbol1 = *abs2.begin();
+		REQUIRE(std::holds_alternative<TARegionState<std::string>>(symbol1));
+		CHECK(std::get<TARegionState<std::string>>(symbol1)
+		      == TARegionState<std::string>("s", "c2", 1));
+		const auto &symbol2 = *std::next(abs2.begin());
+		REQUIRE(std::holds_alternative<ATARegionState<std::string>>(symbol2));
+		CHECK(std::get<ATARegionState<std::string>>(symbol2) == ATARegionState<std::string>(a, 1));
+		const auto &symbol3 = *std::next(abs2.begin(), 2);
+		REQUIRE(std::holds_alternative<ATARegionState<std::string>>(symbol3));
+		CHECK(std::get<ATARegionState<std::string>>(symbol3) == ATARegionState<std::string>(b, 3));
+	}
 }
 
 } // namespace
