d892c7c6c8f0693ead4218f8821a182e4b47b4f7
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-01-08 12:43:29 +0100

    Add function to get a canonical TA for a region config

--- src/automata/include/automata/ta_regions.h
+++ src/automata/include/automata/ta_regions.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "automata.h"
+#include "automata/ta.h"
 #include "utilities/numbers.h"
 
 #include <iostream>
 
 namespace automata {
 namespace ta {
+
+using RegionIndex        = size_t;
+using RegionSetValuation = std::map<std::string, RegionIndex>;
+template <typename LocationT>
+using RegionalizedConfiguration = std::pair<LocationT, RegionSetValuation>;
 
 /// A set of one-dimensional regions
 struct TimedAutomatonRegions
@@ -29,6 +35,24 @@ struct TimedAutomatonRegions
 		}
 	}
 };
+
+/// Get a (unregionalized) Configuration of a TimedAutomaton for a given regionalized Configuration
+template <typename LocationT>
+Configuration<LocationT>
+get_region_candidate(const RegionalizedConfiguration<LocationT> &regionalized_configuration)
+{
+	Configuration<LocationT> res;
+	res.first = regionalized_configuration.first;
+	std::transform(std::begin(regionalized_configuration.second),
+	               std::end(regionalized_configuration.second),
+	               std::inserter(res.second, res.second.end()),
+	               [&](const std::pair<std::string, RegionIndex> &clock_region) {
+		               const std::string &clock_name = clock_region.first;
+		               const RegionIndex &region     = clock_region.second;
+		               return std::make_pair(clock_name, static_cast<ClockValuation>(region) / 2);
+	               });
+	return res;
+}
 
 } // namespace ta
 } // namespace automata
--- test/test_ta_region.cpp
+++ test/test_ta_region.cpp
@@ -37,4 +37,19 @@ TEST_CASE("Region index", "[taRegion]")
 	CHECK(regionSet.getRegionIndex(0.1) == std::size_t(1));
 }
 
+TEST_CASE("Region Candidate", "[taRegion]")
+{
+	const auto candidate =
+	  get_region_candidate<std::string>({"s0", {{"c0", 2}, {"c1", 3}, {"c2", 0}}});
+	CHECK(candidate.first == "s0");
+	const auto &clock_set_valuation = candidate.second;
+	REQUIRE(clock_set_valuation.find("c0") != clock_set_valuation.end());
+	CHECK_THAT(clock_set_valuation.at("c0"), Catch::Matchers::WithinULP(1.0, 4));
+	REQUIRE(clock_set_valuation.find("c1") != clock_set_valuation.end());
+	CHECK(clock_set_valuation.at("c1") > 1.0);
+	CHECK(clock_set_valuation.at("c1") < 2.0);
+	REQUIRE(clock_set_valuation.find("c2") != clock_set_valuation.end());
+	CHECK_THAT(clock_set_valuation.at("c2"), Catch::Matchers::WithinULP(0.0, 4));
+}
+
 } // namespace
