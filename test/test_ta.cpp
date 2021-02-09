1bdedfc67db52f35b073ceab53ea00a12152f4f1
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-02-09 13:00:07 +0100

    ta: use CHECK instead of REQUIRE in tests

--- test/test_ta.cpp
+++ test/test_ta.cpp
@@ -36,33 +36,33 @@ TEST_CASE("Clock constraints with integers", "[ta]")
 	using EQ = AtomicClockConstraintT<std::equal_to<Time>>;
 	using GE = AtomicClockConstraintT<std::greater_equal<Time>>;
 	using GT = AtomicClockConstraintT<std::greater<Time>>;
-	REQUIRE(LT(1).is_satisfied(0));
-	REQUIRE(!LT(1).is_satisfied(1));
-	REQUIRE(!LT(1).is_satisfied(2));
-	REQUIRE(LE(1).is_satisfied(0));
-	REQUIRE(LE(1).is_satisfied(1));
-	REQUIRE(!LE(1).is_satisfied(2));
-	REQUIRE(!EQ(1).is_satisfied(0));
-	REQUIRE(EQ(1).is_satisfied(1));
-	REQUIRE(!EQ(1).is_satisfied(2));
-	REQUIRE(!GE(1).is_satisfied(0));
-	REQUIRE(GE(1).is_satisfied(1));
-	REQUIRE(GE(1).is_satisfied(2));
-	REQUIRE(!GT(1).is_satisfied(0));
-	REQUIRE(!GT(1).is_satisfied(1));
-	REQUIRE(GT(1).is_satisfied(2));
+	CHECK(LT(1).is_satisfied(0));
+	CHECK(!LT(1).is_satisfied(1));
+	CHECK(!LT(1).is_satisfied(2));
+	CHECK(LE(1).is_satisfied(0));
+	CHECK(LE(1).is_satisfied(1));
+	CHECK(!LE(1).is_satisfied(2));
+	CHECK(!EQ(1).is_satisfied(0));
+	CHECK(EQ(1).is_satisfied(1));
+	CHECK(!EQ(1).is_satisfied(2));
+	CHECK(!GE(1).is_satisfied(0));
+	CHECK(GE(1).is_satisfied(1));
+	CHECK(GE(1).is_satisfied(2));
+	CHECK(!GT(1).is_satisfied(0));
+	CHECK(!GT(1).is_satisfied(1));
+	CHECK(GT(1).is_satisfied(2));
 }
 
 TEST_CASE("Simple TA", "[ta]")
 {
 	TimedAutomaton<std::string, std::string> ta{{"a", "b"}, "s0", {"s0"}};
 	ta.add_transition(Transition<std::string, std::string>("s0", "a", "s0"));
-	REQUIRE(ta.accepts_word({}));
-	REQUIRE(ta.accepts_word({{"a", 0}}));
-	REQUIRE(ta.accepts_word({{"a", 1}}));
-	REQUIRE(ta.accepts_word({{"a", 1}, {"a", 1}, {"a", 1}, {"a", 1}}));
-	REQUIRE(!ta.accepts_word({{"b", 0}}));
-	REQUIRE(!ta.accepts_word({{"a", 1}, {"a", 0}}));
+	CHECK(ta.accepts_word({}));
+	CHECK(ta.accepts_word({{"a", 0}}));
+	CHECK(ta.accepts_word({{"a", 1}}));
+	CHECK(ta.accepts_word({{"a", 1}, {"a", 1}, {"a", 1}, {"a", 1}}));
+	CHECK(!ta.accepts_word({{"b", 0}}));
+	CHECK(!ta.accepts_word({{"a", 1}, {"a", 0}}));
 }
 
 TEST_CASE("TA with a simple guard", "[ta]")
@@ -71,9 +71,9 @@ TEST_CASE("TA with a simple guard", "[ta]")
 	ClockConstraint                          c = AtomicClockConstraintT<std::less<Time>>(1);
 	ta.add_clock("x");
 	ta.add_transition(Transition<std::string, std::string>("s0", "a", "s0", {{"x", c}}));
-	REQUIRE(!ta.accepts_word({{"a", 2}}));
-	REQUIRE(ta.accepts_word({{"a", 0.5}}));
-	REQUIRE(!ta.accepts_word({{"a", 1}}));
+	CHECK(!ta.accepts_word({{"a", 2}}));
+	CHECK(ta.accepts_word({{"a", 0.5}}));
+	CHECK(!ta.accepts_word({{"a", 1}}));
 }
 
 TEST_CASE("TA with clock reset", "[ta]")
@@ -82,8 +82,8 @@ TEST_CASE("TA with clock reset", "[ta]")
 	ClockConstraint                          c = AtomicClockConstraintT<std::less<Time>>(2);
 	ta.add_clock("x");
 	ta.add_transition(Transition<std::string, std::string>("s0", "a", "s0", {{"x", c}}, {"x"}));
-	REQUIRE(ta.accepts_word({{"a", 1}, {"a", 2}, {"a", 3}}));
-	REQUIRE(!ta.accepts_word({{"a", 1}, {"a", 3}, {"a", 3}}));
+	CHECK(ta.accepts_word({{"a", 1}, {"a", 2}, {"a", 3}}));
+	CHECK(!ta.accepts_word({{"a", 1}, {"a", 3}, {"a", 3}}));
 }
 
 TEST_CASE("Simple non-deterministic TA", "[ta]")
@@ -94,7 +94,7 @@ TEST_CASE("Simple non-deterministic TA", "[ta]")
 	ta.add_transition(Transition<std::string, std::string>("s0", "a", "s2"));
 	ta.add_transition(Transition<std::string, std::string>("s1", "b", "s1"));
 	ta.add_transition(Transition<std::string, std::string>("s2", "b", "s2"));
-	REQUIRE(ta.accepts_word({{"a", 1}, {"b", 2}}));
+	CHECK(ta.accepts_word({{"a", 1}, {"b", 2}}));
 }
 
 TEST_CASE("Non-determinstic TA with clocks", "[ta]")
@@ -108,13 +108,13 @@ TEST_CASE("Non-determinstic TA with clocks", "[ta]")
 	ta.add_transition(Transition<std::string, std::string>("s0", "a", "s2"));
 	ta.add_transition(Transition<std::string, std::string>("s1", "b", "s1", {{"x", c1}}));
 
-	REQUIRE(ta.accepts_word({{"a", 1}, {"b", 1}}));
-	REQUIRE(!ta.accepts_word({{"a", 1}, {"b", 3}}));
+	CHECK(ta.accepts_word({{"a", 1}, {"b", 1}}));
+	CHECK(!ta.accepts_word({{"a", 1}, {"b", 3}}));
 
 	ta.add_transition(Transition<std::string, std::string>("s2", "b", "s2", {{"x", c2}}));
 
-	REQUIRE(ta.accepts_word({{"a", 1}, {"b", 1}}));
-	REQUIRE(ta.accepts_word({{"a", 1}, {"b", 3}}));
+	CHECK(ta.accepts_word({{"a", 1}, {"b", 1}}));
+	CHECK(ta.accepts_word({{"a", 1}, {"b", 3}}));
 }
 
 TEST_CASE("Transitions must use the TA's alphabet, locations and clocks", "[ta]")
@@ -124,18 +124,18 @@ TEST_CASE("Transitions must use the TA's alphabet, locations and clocks", "[ta]"
 	ta.add_clock("x");
 
 	ClockConstraint c = AtomicClockConstraintT<std::less<Time>>(2);
-	REQUIRE_THROWS_AS(ta.add_transition(Transition<std::string, std::string>("s0", "a", "s2")),
-	                  InvalidLocationException<std::string>);
-	REQUIRE_THROWS_AS(ta.add_transition(Transition<std::string, std::string>("s2", "a", "s0")),
-	                  InvalidLocationException<std::string>);
-	REQUIRE_THROWS_AS(ta.add_transition(
-	                    Transition<std::string, std::string>("s0", "a", "s1", {{"y", c}})),
-	                  InvalidClockException);
-	REQUIRE_THROWS_AS(ta.add_transition(
-	                    Transition<std::string, std::string>("s0", "a", "s1", {}, {"y"})),
-	                  InvalidClockException);
-	REQUIRE_THROWS_AS(ta.add_transition(Transition<std::string, std::string>("s0", "c", "s0")),
-	                  InvalidSymbolException);
+	CHECK_THROWS_AS(ta.add_transition(Transition<std::string, std::string>("s0", "a", "s2")),
+	                InvalidLocationException<std::string>);
+	CHECK_THROWS_AS(ta.add_transition(Transition<std::string, std::string>("s2", "a", "s0")),
+	                InvalidLocationException<std::string>);
+	CHECK_THROWS_AS(ta.add_transition(
+	                  Transition<std::string, std::string>("s0", "a", "s1", {{"y", c}})),
+	                InvalidClockException);
+	CHECK_THROWS_AS(ta.add_transition(
+	                  Transition<std::string, std::string>("s0", "a", "s1", {}, {"y"})),
+	                InvalidClockException);
+	CHECK_THROWS_AS(ta.add_transition(Transition<std::string, std::string>("s0", "c", "s0")),
+	                InvalidSymbolException);
 }
 
 TEST_CASE("Create a TA with non-string location types", "[ta]")
@@ -144,9 +144,9 @@ TEST_CASE("Create a TA with non-string location types", "[ta]")
 	ClockConstraint                           c = AtomicClockConstraintT<std::less<Time>>(1);
 	ta.add_clock("x");
 	ta.add_transition(Transition<unsigned int, std::string>(0, "a", 0, {{"x", c}}));
-	REQUIRE(!ta.accepts_word({{"a", 2}}));
-	REQUIRE(ta.accepts_word({{"a", 0.5}}));
-	REQUIRE(!ta.accepts_word({{"a", 1}}));
+	CHECK(!ta.accepts_word({{"a", 2}}));
+	CHECK(ta.accepts_word({{"a", 0.5}}));
+	CHECK(!ta.accepts_word({{"a", 1}}));
 }
 
 TEST_CASE("Get enabled transitions", "[ta]")
