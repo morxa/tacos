8b07cc83974dfacbc62d838f19b68a0638d400ff
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-03-12 09:06:44 +0100

    utilities: add priority thread pool class

---
+++ src/utilities/include/utilities/priority_thread_pool.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *  priority_thread_pool.h - A thread pool with a priority queue
+ *
+ *  Created:   Wed 10 Mar 22:57:22 CET 2021
+ *  Copyright  2021  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.md file.
+ */
+
+#ifndef SRC_UTILITIES_INCLUDE_UTILITIES_PRIORITY_THREAD_POOL_H
+#define SRC_UTILITIES_INCLUDE_UTILITIES_PRIORITY_THREAD_POOL_H
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <utility>
+
+namespace utilities {
+
+template <typename T>
+struct CompareFirstOfPair
+{
+	bool
+	operator()(const T &t1, const T &t2) const
+	{
+		return t1.first < t2.first;
+	}
+};
+
+template <class T = std::pair<int, std::function<void()>>, class Compare = CompareFirstOfPair<T>>
+class ThreadPool
+{
+public:
+	ThreadPool(std::size_t num_threads = std::thread::hardware_concurrency());
+	~ThreadPool();
+	void add_job(T &&job);
+	void stop();
+	void close_queue();
+	void finish();
+
+private:
+	std::vector<std::thread>                        workers;
+	std::priority_queue<T, std::vector<T>, Compare> queue;
+	std::atomic_bool                                stopping{false};
+	std::atomic_bool                                queue_open{true};
+	std::mutex                                      queue_mutex;
+	std::condition_variable                         queue_cond;
+};
+
+} // namespace utilities
+
+#include "priority_thread_pool.hpp"
+
+#endif /* ifndef SRC_UTILITIES_INCLUDE_UTILITIES_PRIORITY_THREAD_POOL_H */
---
+++ src/utilities/include/utilities/priority_thread_pool.hpp
@@ -0,0 +1,126 @@
+/***************************************************************************
+ *  priority_thread_pool.hpp - A thread pool with a priority queue
+ *
+ *  Created:   Wed 10 Mar 23:06:29 CET 2021
+ *  Copyright  2021  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.md file.
+ */
+
+#pragma once
+
+#include "priority_thread_pool.h"
+
+#include <mutex>
+
+namespace utilities {
+
+class QueueClosedException : public std::logic_error
+{
+	// Use base class constructors.
+	using std::logic_error::logic_error;
+};
+
+template <class T, class Compare>
+ThreadPool<T, Compare>::ThreadPool(std::size_t size)
+/*
+: workers(size, std::thread{[this]() {
+            while (!stopping) {
+              std::unique_lock lock{queue_mutex};
+              while (!queue.empty()) {
+                auto [priority, job] = queue.top();
+                queue.pop();
+                lock.unlock();
+                job();
+                lock.lock();
+              }
+              // Wait for the stop signal or a new job.
+              queue_cond.wait(lock, [this] { return stopping || !queue.empty(); });
+            }
+          }})
+          */
+{
+	for (std::size_t i = 0; i < size; ++i) {
+		workers.push_back(std::thread{[this]() {
+			while (!stopping) {
+				std::unique_lock lock{queue_mutex};
+				while (!queue.empty()) {
+					auto [priority, job] = queue.top();
+					queue.pop();
+					lock.unlock();
+					job();
+					lock.lock();
+					if (stopping) {
+						return;
+					}
+				}
+				if (!queue_open) {
+					return;
+				}
+				// Wait for the stop signal or a new job.
+				queue_cond.wait(lock, [this] { return stopping || !queue.empty() || !queue_open; });
+			}
+		}});
+	}
+}
+
+template <class T, class Compare>
+ThreadPool<T, Compare>::~ThreadPool()
+{
+	stop();
+}
+
+template <class T, class Compare>
+void
+ThreadPool<T, Compare>::add_job(T &&job)
+{
+	if (!queue_open) {
+		throw QueueClosedException("Queue is closed!");
+	}
+	std::lock_guard guard{queue_mutex};
+	queue.push(job);
+	queue_cond.notify_one();
+}
+
+template <class T, class Compare>
+void
+ThreadPool<T, Compare>::close_queue()
+{
+	queue_open = false;
+}
+
+template <class T, class Compare>
+void
+ThreadPool<T, Compare>::finish()
+{
+	close_queue();
+	{
+		std::lock_guard guard{queue_mutex};
+		queue_cond.notify_all();
+	}
+	for (auto &worker : workers) {
+		if (worker.joinable()) {
+			worker.join();
+		}
+	}
+}
+
+template <class T, class Compare>
+void
+ThreadPool<T, Compare>::stop()
+{
+	stopping = true;
+	finish();
+}
+
+} // namespace utilities
