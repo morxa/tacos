6877bf0bd39a1e664e6a7b20cfa5b6ffda8c5ed5
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-04-15 12:43:53 +0200

    utilities: add a C++ wrapper for the cgraph graphviz library

---
+++ src/utilities/graphviz/include/utilities/graphviz/graphviz.h
@@ -0,0 +1,112 @@
+/***************************************************************************
+ *  graphviz.h - A C++ thin wrapper for cgraph
+ *
+ *  Created:   Wed 14 Apr 22:32:42 CEST 2021
+ *  Copyright  2021  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.md file.
+ */
+
+#pragma once
+
+#include <graphviz/cgraph.h>
+#include <graphviz/gvc.h>
+
+#include <filesystem>
+#include <string>
+
+namespace utilities::graphviz {
+
+enum class GraphType {
+	Undirected,
+	StrictUndirected,
+	Directed,
+	StrictDirected,
+};
+
+class Graph;
+
+/** A wrapper for a node in graphviz. */
+class Node
+{
+	friend Graph;
+
+public:
+	/** Set a property to the given value.
+	 * @param property The property to set, e.g., label
+	 * @param value The value to set, e.g., the value of the label
+	 */
+	void set_property(const std::string &property, const std::string &value);
+	/** Get the underlying node.
+	 * @return A pointer to the cgraph node.
+	 */
+	Agnode_t *
+	get_agnode() const
+	{
+		return node;
+	}
+
+private:
+	Node(Agnode_t *node);
+
+private:
+	Agnode_t *node;
+};
+
+/** A simple wrapper to create graphs with graphviz. */
+class Graph
+{
+public:
+	/** Create a non-strict directed graph. */
+	Graph();
+	/** Create a graph with a custom name and type.
+	 * @param name The name of the graph.
+	 * @param type the type of the graph.
+	 */
+	Graph(const std::string &name, GraphType type);
+	/** @brief Destructor.
+	 * Frees all resources that were allocated while creating the graph. */
+	~Graph();
+	/** Add a node to the graph.
+	 * @param label The node label
+	 */
+	Node add_node(std::string label);
+	/** Add an edge to the graph.
+	 * If the source or target node does not exist, it will be created.
+	 * @param source The name of the source node of the edge
+	 * @param target The nam eof the target node of the edge
+	 * @param label The edge label
+	 */
+	void add_edge(const Node &source, const Node &target, std::string label = "");
+	/** Create a dot representation of the graph.
+	 * @return The dot representation as string
+	 */
+	std::string to_dot();
+
+	/** Render the graph to a file.
+	 * @param output_path The path to the output file, the suffix must match a suitable graphviz
+	 * engine.
+	 */
+	void render_to_file(const std::filesystem::path &output_path);
+
+private:
+	void        layout();
+	std::string graph_name;
+	Agraph_t *  graph{nullptr};
+	GVC_t *     context{nullptr};
+	bool        layout_created{false};
+	std::size_t last_node_id{0};
+	std::size_t last_edge_id{0};
+};
+
+} // namespace utilities::graphviz
---
+++ test/test_graphviz.cpp
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *  test_graphviz.cpp - Test graphviz wrapper
+ *
+ *  Created:   Thu 15 Apr 10:20:22 CEST 2021
+ *  Copyright  2021  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.md file.
+ */
+
+#include "catch2/matchers/catch_matchers_string.hpp"
+
+#include <utilities/graphviz/graphviz.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace {
+
+using Catch::Matchers::Contains;
+using utilities::graphviz::Graph;
+
+TEST_CASE("Create a graphviz graph", "[utilities][graphviz]")
+{
+	Graph g{};
+	auto  n1 = g.add_node("node 1");
+	auto  n2 = g.add_node("node 2");
+	auto  n3 = g.add_node("node 3");
+	auto  n4 = g.add_node("node 4");
+	g.add_edge(n1, n2);
+	g.add_edge(n2, n3, "foo bar");
+	g.add_edge(n2, n4, "foo baz");
+	const auto dot = g.to_dot();
+	CHECK_THAT(dot, Contains("shape=record"));
+	CHECK_THAT(dot, Contains("\"node 1\""));
+	CHECK_THAT(dot, Contains("\"node 2\""));
+	CHECK_THAT(dot, Contains("\"node 3\""));
+	CHECK_THAT(dot, Contains("\"node 4\""));
+	CHECK_THAT(dot, Contains("1 -> 2"));
+	CHECK_THAT(dot, Contains("2 -> 3"));
+	CHECK_THAT(dot, Contains("2 -> 4"));
+	CHECK_THAT(dot, Contains("label=\"foo bar\""));
+	CHECK_THAT(dot, Contains("label=\"foo baz\""));
+	g.render_to_file("graphviz.png");
+	CHECK_THROWS(g.render_to_file("nosuffix"));
+}
+
+} // namespace
