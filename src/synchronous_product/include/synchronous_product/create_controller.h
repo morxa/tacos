58f0a03339dffa0bcd138dcdbc2749524f87f1a5
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-04-22 15:14:39 +0200

    controller: add all nodes to the controller, not just one

--- src/synchronous_product/include/synchronous_product/create_controller.h
+++ src/synchronous_product/include/synchronous_product/create_controller.h
@@ -29,6 +29,7 @@
 #include <stdexcept>
 namespace controller_synthesis {
 
+namespace details {
 /** Construct a set of constraints from a time successor CanonicalABWord.
  */
 template <typename LocationT, typename ActionT>
@@ -54,10 +55,12 @@ get_constraints_from_time_successor(
 }
 
 template <typename LocationT, typename ActionT>
-automata::ta::TimedAutomaton<std::set<synchronous_product::CanonicalABWord<LocationT, ActionT>>,
-                             ActionT>
-create_controller(const synchronous_product::SearchTreeNode<LocationT, ActionT> *const root,
-                  synchronous_product::RegionIndex                                     K)
+void
+add_node_to_controller(
+  const synchronous_product::SearchTreeNode<LocationT, ActionT> *const node,
+  synchronous_product::RegionIndex                                     K,
+  automata::ta::TimedAutomaton<std::set<synchronous_product::CanonicalABWord<LocationT, ActionT>>,
+                               ActionT> *                              controller)
 {
 	using synchronous_product::NodeLabel;
 	using Transition =
@@ -65,45 +68,51 @@ create_controller(const synchronous_product::SearchTreeNode<LocationT, ActionT> 
 	                           ActionT>;
 	using Location =
 	  automata::ta::Location<std::set<synchronous_product::CanonicalABWord<LocationT, ActionT>>>;
-	auto               node = root;
-	std::set<Location> locations;
-	locations.emplace(root->words);
-	std::set<ActionT>       actions;
-	std::vector<Transition> transitions;
-	std::set<std::string>   clocks;
 	if (node->label != NodeLabel::TOP) {
 		throw std::invalid_argument(
-		  "Cannot create a controller for a search tree that is not labeled with TOP");
+		  "Cannot create a controller for a node that is not labeled with TOP");
 	}
-	while (!node->children.empty()) {
-		auto successor_it =
-		  std::find_if(begin(node->children), end(node->children), [](const auto &child) {
-			  return child->label == NodeLabel::TOP;
-		  });
-		if (successor_it == std::end(node->children)) {
-			break;
+	for (const auto &successor : node->children) {
+		if (successor->label != NodeLabel::TOP) {
+			continue;
 		}
-		auto successor = successor_it->get();
-		locations.emplace(successor->words);
+		controller->add_location(Location{successor->words});
+		controller->add_final_location(Location{successor->words});
 		for (const auto &[region_increment, action] : successor->incoming_actions) {
-			// TODO source/target location and action are done. What about clock constraints and resets?
-			// We need to reconstruct them from the node's words and the child's words.
+			// TODO source/target location and action are done. What about clock constraints and
+			// resets? We need to reconstruct them from the node's words and the child's words.
 			// Problem: We do not know which node word matches which child's node.
 			// Is it sufficient to consider the reg_a components of the words?
-			actions.insert(action);
+			controller->add_action(action);
 			auto constraints = get_constraints_from_time_successor(
 			  get_nth_time_successor(reg_a(*std::begin(node->words)), region_increment, K), K);
 			for (const auto &[clock_name, constraint] : constraints) {
-				clocks.insert(clock_name);
+				controller->add_clock(clock_name);
 			}
-			transitions.push_back(
+			controller->add_transition(
 			  Transition{Location{node->words}, action, Location{successor->words}, constraints, {}});
 		}
-		node = successor;
-	}
-	return automata::ta::TimedAutomaton<
-	  std::set<synchronous_product::CanonicalABWord<LocationT, ActionT>>,
-	  ActionT>{locations, actions, Location{root->words}, locations, clocks, transitions};
+		add_node_to_controller(successor.get(), K, controller);
+	};
+}
+
+} // namespace details
+
+template <typename LocationT, typename ActionT>
+automata::ta::TimedAutomaton<std::set<synchronous_product::CanonicalABWord<LocationT, ActionT>>,
+                             ActionT>
+create_controller(const synchronous_product::SearchTreeNode<LocationT, ActionT> *const root,
+                  synchronous_product::RegionIndex                                     K)
+{
+	using namespace details;
+	using synchronous_product::NodeLabel;
+	using Location =
+	  automata::ta::Location<std::set<synchronous_product::CanonicalABWord<LocationT, ActionT>>>;
+	automata::ta::TimedAutomaton<std::set<synchronous_product::CanonicalABWord<LocationT, ActionT>>,
+	                             ActionT>
+	  controller{{}, Location{root->words}, {}};
+	add_node_to_controller(root, K, &controller);
+	return controller;
 }
 
 } // namespace controller_synthesis
