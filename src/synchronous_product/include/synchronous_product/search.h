cf2f5c82d81626bc79d5860e13794bf0b1779e8a
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2021-04-24 00:54:32 +0200

    search: directly label a node with TOP if it only contains ATA sinks
    If every word in a node contains an ATA sink location, we know that the
    specification can no longer be satisfies. Instead of continuing the
    search, directly label the node with GOOD.

--- src/synchronous_product/include/synchronous_product/search.h
+++ src/synchronous_product/include/synchronous_product/search.h
@@ -37,6 +37,7 @@
 #include <limits>
 #include <memory>
 #include <queue>
+#include <variant>
 
 namespace synchronous_product {
 /** Search the configuration tree for a valid controller. */
@@ -97,6 +98,30 @@ public:
 	get_root() const
 	{
 		return tree_root_.get();
+	}
+
+	/** @brief Check if the node has a satisfiable ATA configuration.
+	 * If every word in the node contains an ATA sink location, than none of those configurations is
+	 * satisfiable.
+	 * @return false if every word contains an ATA sink location
+	 */
+	bool
+	has_satisfiable_ata_configuration(Node *node) const
+	{
+		return !std::all_of(std::begin(node->words), std::end(node->words), [](const auto &word) {
+			return std::any_of(std::begin(word), std::end(word), [](const auto &component) {
+				return std::find_if(std::begin(component),
+				                    std::end(component),
+				                    [](const auto &region_symbol) {
+					                    return std::holds_alternative<ATARegionState<ActionType>>(
+					                             region_symbol)
+					                           && std::get<ATARegionState<ActionType>>(region_symbol).formula
+					                                == logic::MTLFormula<ActionType>{
+					                                  logic::AtomicProposition<ActionType>{"sink"}};
+				                    })
+				       != std::end(component);
+			});
+		});
 	}
 
 	/** Check if a node is bad, i.e., if it violates the specification.
@@ -186,7 +211,7 @@ public:
 			}
 			return;
 		}
-		if (dominates_ancestor(node)) {
+		if (!has_satisfiable_ata_configuration(node) || dominates_ancestor(node)) {
 			node->state       = NodeState::GOOD;
 			node->is_expanded = true;
 			if (incremental_labeling_) {
