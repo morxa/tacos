b39a3f644654f36a5a273676d2494c6f9c3e612a
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2022-03-24 19:59:48 +0100

    Switch license to LGPLv3+ and use SPDX identifiers

--- src/mtl_ata_translation/include/mtl_ata_translation/translator.hpp
+++ src/mtl_ata_translation/include/mtl_ata_translation/translator.hpp
@@ -3,20 +3,10 @@
  *
  *  Created: Thu 18 Jun 2020 11:21:13 CEST 11:21
  *  Copyright  2020  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ *  SPDX-License-Identifier: LGPL-3.0-or-later
  ****************************************************************************/
 
-/*  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  Read the full text in the LICENSE.GPL file in the doc directory.
- */
+
 
 #pragma once
 
--- test/test_xml_writer.cpp
+++ test/test_xml_writer.cpp
@@ -3,19 +3,9 @@
  *
  *  Created:   Thu 22 Jul 12:01:49 CEST 2021
  *  Copyright  2021  Stefan Schupp <stefan.schupp@tuwien.ac.at>
+ *  SPDX-License-Identifier: LGPL-3.0-or-later
  ****************************************************************************/
-/*  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  Read the full text in the LICENSE.md file.
- */
+
 
 #include "io/XmlWriter.h"
 
