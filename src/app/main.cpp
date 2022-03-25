7e39f2218d910a7d5a2765e128aa3a4d8009c289
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2022-03-25 10:08:56 +0100

    Switch license to LGPLv3+ and use SPDX identifiers

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,19 +3,9 @@
  *
  *  Created:   Mon 19 Apr 16:20:10 CEST 2021
  *  Copyright  2021  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ *  SPDX-License-Identifier: LGPL-3.0-or-later
  ****************************************************************************/
-/*  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  Read the full text in the LICENSE.md file.
- */
+
 
 #include "app/app.h"
 
--- test/fischer.h
+++ test/fischer.h
@@ -3,20 +3,10 @@
  *
  *  Created: Wed 21 Apr 2021 15:07:42 CEST 14:00
  *  Copyright  2021  Stefan Schupp <stefan.schupp@cs.rwth-aachen.de>
+ *  SPDX-License-Identifier: LGPL-3.0-or-later
  ****************************************************************************/
 
-/*  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  Read the full text in the LICENSE.GPL file in the doc directory.
- */
+
 
 #pragma once
 
