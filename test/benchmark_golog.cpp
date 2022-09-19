1bf82fd95fe448d41a62e19d4c5126cff044b256
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2022-09-19 10:09:26 +0200

    golog robot: add benchmark
    Benchmark the different heuristics in the Golog Robot scenario.

---
+++ test/benchmark_golog.cpp
@@ -0,0 +1,11 @@
+/***************************************************************************
+ *  benchmark_golog.cpp - Main benchmark binary for Golog benchmarks
+ *
+ *  Created:   Wed 28 Jul 15:57:02 CEST 2021
+ *  Copyright  2021  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ *  SPDX-License-Identifier: LGPL-3.0-or-later
+ ****************************************************************************/
+
+#include <benchmark/benchmark.h>
+
+BENCHMARK_MAIN();
