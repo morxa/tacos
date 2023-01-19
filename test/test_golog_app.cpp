99b3b08b7124826c3d6c447771e7c1c73fd02f2d
Daniel Swoboda <swoboda@kbsg.rwth-aachen.de>
2023-01-19 13:36:17 +0100

    golog-app: add more test-cases to increase coverage
    Cases are once again based on the tests for the main app for TAs.

--- test/test_golog_app.cpp
+++ test/test_golog_app.cpp
@@ -16,50 +16,82 @@ TEST_CASE("Launch the main application", "[app]")
 {
 	const std::filesystem::path test_data_dir =
 	  std::filesystem::current_path() / "data" / "golog_app";
-	const std::filesystem::path          plant_path = test_data_dir / "robot_camera.gpp";
-	const std::filesystem::path          spec_path  = test_data_dir / "robot_camera_spec.pbtxt";
-	const std::filesystem::path          controller_dot_path   = test_data_dir / "controller.png";
-	const std::filesystem::path          controller_proto_path = test_data_dir / "controller.pbtxt";
-	const std::filesystem::path          tree_dot_graph        = test_data_dir / "tree.png";
-	constexpr const int                  argc                  = 30;
-	const std::array<const char *, argc> argv{"app",
-	                                          "--program",
-	                                          plant_path.c_str(),
-	                                          "--spec",
-	                                          spec_path.c_str(),
-	                                          "--k",
-	                                          "2",
-	                                          "-c",
-	                                          "start(drive(machine1, machine2))",
-	                                          "-c",
-	                                          "start(grasp(machine2, obj1))",
-	                                          "-c",
-	                                          "start(boot_camera())",
-	                                          "-c",
-	                                          "start(shutdown_camera())",
-	                                          "-e",
-	                                          "end(drive(machine1, machine2))",
-	                                          "-e",
-	                                          "end(grasp(machine2, obj1))",
-	                                          "-e",
-	                                          "end(boot_camera())",
-	                                          "-e",
-	                                          "end(shutdown_camera())",
-	                                          "--visualize-search-tree",
-	                                          tree_dot_graph.c_str(),
-	                                          "--visualize-controller",
-	                                          controller_dot_path.c_str(),
-	                                          "--hide-controller-labels",
-	                                          "-o",
-	                                          controller_proto_path.c_str()};
-	tacos::golog_app::Launcher           launcher{argc, argv.data()};
-	launcher.run();
-	CHECK(std::filesystem::exists(controller_dot_path));
-	std::filesystem::remove(controller_dot_path);
-	CHECK(std::filesystem::exists(controller_proto_path));
-	std::filesystem::remove(controller_proto_path);
-	CHECK(std::filesystem::exists(tree_dot_graph));
-	std::filesystem::remove(tree_dot_graph);
+	const std::filesystem::path plant_path            = test_data_dir / "robot_camera.gpp";
+	const std::filesystem::path spec_path             = test_data_dir / "robot_camera_spec.pbtxt";
+	const std::filesystem::path controller_dot_path   = test_data_dir / "controller.png";
+	const std::filesystem::path controller_proto_path = test_data_dir / "controller.pbtxt";
+	const std::filesystem::path tree_dot_graph        = test_data_dir / "tree.png";
+
+	std::vector<const char *> basic_parameters{"golog_app-exc",
+	                                           "--program",
+	                                           plant_path.c_str(),
+	                                           "--spec",
+	                                           spec_path.c_str(),
+	                                           "--k",
+	                                           "2",
+	                                           "-c",
+	                                           "start(drive(machine1, machine2))",
+	                                           "-c",
+	                                           "start(grasp(machine2, obj1))",
+	                                           "-c",
+	                                           "start(boot_camera())",
+	                                           "-c",
+	                                           "start(shutdown_camera())",
+	                                           "-e",
+	                                           "end(drive(machine1, machine2))",
+	                                           "-e",
+	                                           "end(grasp(machine2, obj1))",
+	                                           "-e",
+	                                           "end(boot_camera())",
+	                                           "-e",
+	                                           "end(shutdown_camera())"};
+
+	SECTION("Simple Launch")
+	{
+		std::vector<const char *>  argv(basic_parameters);
+		tacos::golog_app::Launcher launcher{(int)argv.size(), argv.data()};
+		CHECK_NOTHROW(launcher.run());
+	}
+
+	SECTION("Select heuristics")
+	{
+		for (const auto &heuristic : {"bfs", "dfs", "composite", "random", "time"}) {
+			std::vector<const char *> argv(basic_parameters);
+			std::vector<const char *> heuristic_params{"--heuristic", heuristic};
+			argv.insert(argv.end(), heuristic_params.begin(), heuristic_params.end());
+
+			tacos::golog_app::Launcher launcher{(int)argv.size(), argv.data()};
+			CHECK_NOTHROW(launcher.run());
+		}
+	}
+
+	SECTION("Visualizations")
+	{
+		std::vector<const char *> argv(basic_parameters);
+		std::vector<const char *> visualize_params{"--visualize-controller",
+		                                           controller_dot_path.c_str(),
+		                                           "--hide-controller-labels",
+		                                           "--visualize-search-tree",
+		                                           tree_dot_graph.c_str()};
+		argv.insert(argv.end(), visualize_params.begin(), visualize_params.end());
+		tacos::golog_app::Launcher launcher{(int)argv.size(), argv.data()};
+		CHECK_NOTHROW(launcher.run());
+		CHECK(std::filesystem::exists(controller_dot_path));
+		std::filesystem::remove(controller_dot_path);
+		CHECK(std::filesystem::exists(tree_dot_graph));
+		std::filesystem::remove(tree_dot_graph);
+	}
+
+	SECTION("Create controller proto")
+	{
+		std::vector<const char *> argv(basic_parameters);
+		std::vector<const char *> controller_params{"-o", controller_proto_path.c_str()};
+		argv.insert(argv.end(), controller_params.begin(), controller_params.end());
+		tacos::golog_app::Launcher launcher{(int)argv.size(), argv.data()};
+		CHECK_NOTHROW(launcher.run());
+		CHECK(std::filesystem::exists(controller_proto_path));
+		std::filesystem::remove(controller_proto_path);
+	}
 }
 
 TEST_CASE("Running the golog app with invalid input", "[app]")
