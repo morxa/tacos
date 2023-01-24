6477734759111731df48913868a6fda9247bd9a3
Daniel Swoboda <swoboda@kbsg.rwth-aachen.de>
2023-01-24 21:13:17 +0100

    golog-verification: add files for path search implementation
    The idea of verification for Golog programs is that if the tree search
    for a given program + MTL specification sans controller actions (i.e.
    every action is an environment action), leads to the root node being
    labelled as top, then no execution of the Golog program violates the
    given specification, i.e. the program is safe against the specification.
    On the other hand if the resulting tree labels the root node as bottom,
    then there is at least one unsafe execution path of the program. In this
    case, finding the path gives a counter-example against the MTL spec. A
    simple path traversal on a fully built search tree towards a top or
    bottom labelled leaf node is to be implemented in these files, to
    enable gocos to do Golog program verification.

---
+++ src/golog_adapter/golog_path_search.cpp
@@ -0,0 +1,17 @@
+/***************************************************************************
+ *  golog_path_search.cpp - Utility to find paths in Golog search trees
+ *  from the root node to a top or bottom labelled leaf node.
+ *
+ *  Created:   Tue 24 Jan 20:53:00 CET 2023
+ *  Copyright  2023  Daniel Swoboda <swoboda@kbsg.rwth-aachen.de>
+ *  SPDX-License-Identifier: LGPL-3.0-or-later
+ ****************************************************************************/
+
+#include "golog_adapter/golog_symbols.h"
+
+#include <regex>
+#include <stdexcept>
+
+namespace tacos::search {
+
+} // namespace tacos::search
---
+++ src/golog_adapter/include/golog_adapter/golog_path_search.h
@@ -0,0 +1,17 @@
+/***************************************************************************
+ *  golog_path_search.h - Utility to find paths in Golog search trees
+ *  from the root node to a top or bottom labelled leaf node.
+ *
+ *  Created:   Tue 24 Jan 20:53:00 CET 2023
+ *  Copyright  2023  Daniel Swoboda <swoboda@kbsg.rwth-aachen.de>
+ *  SPDX-License-Identifier: LGPL-3.0-or-later
+ ****************************************************************************/
+
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+namespace tacos::search {
+
+} // namespace tacos::search
