413826b5230249bfb364178b02c8c8c74fb8ff5d
Daniel Swoboda <swoboda@kbsg.rwth-aachen.de>
2023-03-06 12:08:05 +0100

    golog-app: add tree search for counter example generation
    In verification mode, we want to generate a possibly minimal counter
    example of an execution trace of a golog program violating the given
    constraints. To achieve this, we traverse the search tree until we
    reach a bottom labelled node.

--- src/golog_adapter/golog_path_search.cpp
+++ src/golog_adapter/golog_path_search.cpp
@@ -7,11 +7,75 @@
  *  SPDX-License-Identifier: LGPL-3.0-or-later
  ****************************************************************************/
 
-#include "golog_adapter/golog_symbols.h"
+#include "golog_adapter/golog_path_search.h"
 
-#include <regex>
-#include <stdexcept>
+#include "search/create_controller.h"
 
 namespace tacos::search {
+
+void
+traverse_tree(tacos::search::TreeSearch<tacos::search::GologLocation,
+                                        std::string,
+                                        std::string,
+                                        true,
+                                        tacos::search::GologProgram,
+                                        true>                                &search_tree,
+              automata::ta::TimedAutomaton<std::set<GologWord>, GologAction> *controller)
+{
+	std::cout << search_tree.get_size() << std::endl;
+
+	traverse_node(*search_tree.get_root(), 0, tacos::search::NodeLabel::BOTTOM, controller);
+}
+
+void
+traverse_node(Node                                                           &node,
+              int                                                             offset,
+              tacos::search::NodeLabel                                        traverse_label,
+              automata::ta::TimedAutomaton<std::set<GologWord>, GologAction> *controller)
+{
+	for (const auto &[timed_action, child] : node.get_children()) {
+		if (child->label == traverse_label) {
+			bool new_location = controller->add_location(TALocation{child->words});
+			controller->add_final_location(TALocation{child->words});
+
+			for (const auto &[action, constraints] :
+			     tacos::controller_synthesis::details::get_constraints_from_outgoing_action(node.words,
+			                                                                                timed_action,
+			                                                                                2)) {
+				for (const auto &[clock, _constraint] : constraints) {
+					controller->add_clock(clock);
+				}
+				controller->add_action(action);
+				controller->add_transition(
+				  TATransition{TALocation{node.words}, action, TALocation{child->words}, constraints, {}});
+			}
+
+			// continue to go down on the tree, if found location is new
+			if (new_location) {
+				traverse_node(*child, offset + 1, traverse_label, controller);
+			}
+			// keep counter-example minimal
+			break;
+		}
+	}
+}
+
+automata::ta::TimedAutomaton<std::set<GologWord>, GologAction>
+verify_program(tacos::search::TreeSearch<tacos::search::GologLocation,
+                                         std::string,
+                                         std::string,
+                                         true,
+                                         tacos::search::GologProgram,
+                                         true> &search_tree)
+{
+	if (search_tree.get_root()->label == tacos::search::NodeLabel::TOP) {
+		throw std::runtime_error("Program is safe, can't create counter-example.");
+	} else {
+		automata::ta::TimedAutomaton<std::set<GologWord>, GologAction> controller{
+		  {}, TALocation{search_tree.get_root()->words}, {}};
+		traverse_tree(search_tree, &controller);
+		return controller;
+	}
+}
 
 } // namespace tacos::search
--- src/golog_adapter/include/golog_adapter/golog_path_search.h
+++ src/golog_adapter/include/golog_adapter/golog_path_search.h
@@ -7,11 +7,59 @@
  *  SPDX-License-Identifier: LGPL-3.0-or-later
  ****************************************************************************/
 
-#include <set>
+#include "golog_adapter/golog_adapter.h"
+#include "search/search.h"
+#include "search/search_tree.h"
+
+#include <iostream>
 #include <string>
-#include <tuple>
-#include <vector>
 
 namespace tacos::search {
+
+using Node            = SearchTreeNode<tacos::search::GologLocation, std::string, std::string>;
+using GologAction     = std::string;
+using GologConstraint = std::string;
+using GologWord       = search::CanonicalABWord<tacos::search::GologLocation, GologConstraint>;
+using TALocation      = automata::ta::Location<std::set<GologWord>>;
+using TATransition    = automata::ta::Transition<std::set<GologWord>, GologAction>;
+/**
+ * @brief
+ *
+ * @param test
+ */
+void traverse_tree(tacos::search::TreeSearch<tacos::search::GologLocation,
+                                             std::string,
+                                             std::string,
+                                             true,
+                                             tacos::search::GologProgram,
+                                             true> &search_tree,
+                   automata::ta::TimedAutomaton<
+                     std::set<search::CanonicalABWord<tacos::search::GologLocation, std::string>>,
+                     std::string> *controller);
+
+/**
+ * @brief
+ *
+ * @param node
+ */
+void traverse_node(Node                    &node,
+                   int                      offset,
+                   tacos::search::NodeLabel traverse_label,
+                   automata::ta::TimedAutomaton<
+                     std::set<search::CanonicalABWord<tacos::search::GologLocation, std::string>>,
+                     std::string> *controller);
+
+/**
+ * @brief
+ *
+ * @param search_tree
+ */
+automata::ta::TimedAutomaton<std::set<GologWord>, GologAction>
+verify_program(tacos::search::TreeSearch<tacos::search::GologLocation,
+                                         std::string,
+                                         std::string,
+                                         true,
+                                         tacos::search::GologProgram,
+                                         true> &search_tree);
 
 } // namespace tacos::search
